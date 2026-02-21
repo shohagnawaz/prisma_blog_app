a3edc5dd66b9daf2e3a2f8d53500e8767e1ef99f
shohagnawaz <snawazdigital@gmail.com>
2026-02-21 18:46:07 +0600

    auth

---
+++ src/modules/post/post.router.ts
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { postController } from "./post.controller";
+
+const router = express.Router();
+
+router.post("/", postController.createPost);
+
+export const postRouter: Router = router;
